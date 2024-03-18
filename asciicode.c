69a57c9c4015fe6ba938f41bff05b911c02a32ea
suditheboss <sdpthere@gmail.com>
2024-03-18 16:57:55 +0530

    18/03

---
+++ asciicode.c
@@ -0,0 +1,11 @@
+//wap to take a char from user and print ascci code.
+#include<stdio.h>
+void main()
+{
+	char ch;
+	printf("enter a char : ");
+	scanf("%c",&ch);
+	printf("value of ch  : %c\n",ch);
+	printf("ascii code of %c = %d",ch,ch);
+		
+}
---
+++ charinputuser.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	char ch,ch1;
+	ch='A';
+	printf("enter a char : ");
+	scanf("%c",&ch1);
+	printf("value of ch  : %c\n",ch);
+	printf("value of ch1 : %c\n",ch1);
+		
+}
