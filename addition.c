2813a23368051e73087a2ac63f32cddaff3bc486
suditheboss <sdpthere@gmail.com>
2024-03-18 16:27:34 +0530

    18/03

---
+++ addition.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	printf("addition programm : -\n");
+	int num1,num2,sum;
+	printf("enter frist number : ");
+	scanf("%d",&num1);
+	printf("enter second number : ");
+	scanf("%d",&num2);
+	printf("value of num1 = %d \n",num1);
+	printf("value of num2 = %d \n",num2);
+	sum=num1+num2;
+	printf("sum of %d and %d = %d",num1,num2,sum);
+	
+}
