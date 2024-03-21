ef46add536cdf907c5ea1ed207478fc6396e9cc5
ChetanPatel1999 <chetan8085patel@gmail.com>
2024-03-21 17:03:26 +0530

    21/01

---
+++ airtmeticoperator.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int a=13,b=5;
+	int ans;
+	ans=a+b;
+	printf("a + b = %d\n",ans);//18
+	ans=a-b;
+	printf("a - b = %d\n",ans);//8
+	ans=a/b;
+	printf("a / b = %d\n",ans);// 2
+	ans=a%b;
+	printf("a mod b = %d\n",ans);// 3
+	
+}
---
+++ datatype.c
@@ -0,0 +1,19 @@
+// data types size and range
+// sizeof operator
+#include<stdio.h>
+void main()
+{
+/* short int 	si;// integer value
+ int i;// integer value
+ char ch;// character value
+ double d;// decimal value store
+ printf("size of short int = %d\n",sizeof(si));
+ printf("size of  int = %d\n",sizeof(i));
+ printf("size of char = %d\n",sizeof(ch));
+ printf("size of double = %d\n",sizeof(d));*/
+ //unsigned char ch=257;
+// printf("%d",ch);
+ unsigned short int a=655;
+  printf("%d\n",a);
+  printf("adress of a = %d",&a);
+}
