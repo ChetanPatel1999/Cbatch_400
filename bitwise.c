547e44bb1261ecd040706ec8053f2d01baf7ec66
ChetanPatel1999 <chetan8085patel@gmail.com>
2024-04-02 12:30:13 +0530

    01/04

---
+++ bitwise.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int a=12,b=3;
+	int ans;
+	ans=a & b;
+	printf("a & b = %d\n",ans);
+		ans=a | b;
+	printf("a | b = %d\n",ans);
+	ans=a ^ b;
+	printf("a ^ b = %d\n",ans);
+	ans=a<< b;
+	printf("a << b = %d\n",ans);
+	ans=a>> b;
+	printf("a >> b = %d\n",ans);
+}
